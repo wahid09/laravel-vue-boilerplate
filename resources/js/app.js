78a8e1a48a35c3ca7b2ab5aef41639326ecd98d6
wahid ullah <wahid.ullah.cse@gmail.com>
2023-09-25 21:25:32 +0600

    basic setup

---
+++ resources/js/app.js
@@ -0,0 +1,21 @@
+import './bootstrap';
+
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
+
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from './routes';
+
+
+const app = createApp({
+});
+
+const router = createRouter({
+    routes: routes,
+    history: createWebHistory(),
+});
+
+app.use(router);
+app.mount('#app');
